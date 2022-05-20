cc7e67b8622661166f3e5c60cfaea3842a87f804
Lukas Lindstedt <0305lilu@jonkoping.se>
2022-05-20 15:49:46 +0200

    h

--- Slutprojekt_lukas_lindstedt/Form1.Designer.cs
+++ Slutprojekt_lukas_lindstedt/Form1.Designer.cs
@@ -29,21 +29,60 @@ namespace Slutprojekt_lukas_lindstedt
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(172, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 268);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(289, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Slutprojekt_lukas_lindstedt/Form1.cs
+++ Slutprojekt_lukas_lindstedt/Form1.cs
@@ -17,13 +17,62 @@ namespace Slutprojekt_lukas_lindstedt
         public Form1()
         {
             InitializeComponent();
+
         }
+
+        int[,] ruta = new int[8, 8];
+        int[,] minevärde = new int[8, 8];
+        int i = 0;
+        int j = 0;
+        string antalminor = "";
+        int svårighetsgrad = 0;
+        Font minefont = new Font("Calibri", 8);
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ruta[2,4] = 1;
 
 
+        }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush N = new SolidBrush(Color.LightSlateGray);
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (ruta[i,j] == 0)
+                    {
+                        g.FillRectangle(N, i * 40, j * 40, 30, 30);
+                        g.DrawString(antalminor,minefont ,N , i * 40, j *40);
+                    }
+                    
+                }
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            Random kordinat = new Random();
+            while (x <= svårighetsgrad)
+            {
+                //random kordinat här
+                //if(kordinaten redan har mina)
+            }
+            //en while sats används ifall att man får en kordinat som redan har en mina
+            //så kan man skriva så denna loopen inte räknas.
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            panel1.Invalidate();
         }
     }
 }
