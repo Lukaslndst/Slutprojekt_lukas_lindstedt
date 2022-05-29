68e250df2367ab3f1a74b5c069d02770328ee055
Lukas Lindstedt <0305lilu@jonkoping.se>
2022-05-29 19:26:06 +0200

    b

--- Slutprojekt_lukas_lindstedt/Form1.Designer.cs
+++ Slutprojekt_lukas_lindstedt/Form1.Designer.cs
@@ -31,7 +31,12 @@ namespace Slutprojekt_lukas_lindstedt
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,39 +44,84 @@ namespace Slutprojekt_lukas_lindstedt
             // 
             this.panel1.Location = new System.Drawing.Point(172, 77);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(481, 268);
+            this.panel1.Size = new System.Drawing.Size(481, 343);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(172, 23);
+            this.button1.Location = new System.Drawing.Point(363, 26);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Rig Mines";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(30, 311);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(108, 108);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "debug";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "show all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(289, 12);
+            this.trackBar1.Location = new System.Drawing.Point(172, 26);
+            this.trackBar1.Maximum = 32;
+            this.trackBar1.Minimum = 5;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Size = new System.Drawing.Size(126, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(24, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of Mines";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -82,7 +132,11 @@ namespace Slutprojekt_lukas_lindstedt
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Slutprojekt_lukas_lindstedt/Form1.cs
+++ Slutprojekt_lukas_lindstedt/Form1.cs
@@ -24,21 +24,21 @@ namespace Slutprojekt_lukas_lindstedt
         int[,] minevärde = new int[8, 8];
         int i = 0;
         int j = 0;
-        string antalminor = "";
-        int svårighetsgrad = 0;
-        Font minefont = new Font("Calibri", 8);
+        int näraminor = 0;
+        int antalminor = 5;
+        Font minefont = new Font("Calibri", 20);
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ruta[2,4] = 1;
-
 
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            SolidBrush N = new SolidBrush(Color.LightSlateGray);
+            SolidBrush Blank = new SolidBrush(Color.LightSlateGray);
+            SolidBrush Flag = new SolidBrush(Color.Red);
+
 
             for (int i = 0; i < 8; i++)
             {
@@ -46,10 +46,40 @@ namespace Slutprojekt_lukas_lindstedt
                 {
                     if (ruta[i,j] == 0)
                     {
-                        g.FillRectangle(N, i * 40, j * 40, 30, 30);
-                        g.DrawString(antalminor,minefont ,N , i * 40, j *40);
+                        g.FillRectangle(Blank, i * 40, j * 40, 30, 30);
                     }
-                    
+                    if (ruta[i, j] == 1)
+                    {
+                        if (minevärde[i,j] == 0)
+                        {
+
+                            näraminor = 0;
+                            if (i-1 >= 0)
+                            {
+                                näraminor = näraminor + minevärde[i - 1, j];
+                                if (j-1 >= 0) { näraminor = näraminor + minevärde[i - 1, j - 1]; }
+                                if (j+1 < 8) { näraminor = näraminor + minevärde[i - 1, j + 1]; }
+                            }
+                            if (i+1 < 8)
+                            {
+                                näraminor = näraminor + minevärde[i + 1, j];
+                                if (j - 1 >= 0) { näraminor = näraminor + minevärde[i + 1, j - 1]; }
+                                if (j + 1 < 8) { näraminor = näraminor + minevärde[i + 1, j + 1]; }
+                            }
+                            if (j-1 >= 0) { näraminor = näraminor + minevärde[i, j - 1]; }
+                            if (j+1 < 8) { näraminor = näraminor + minevärde[i, j + 1]; }
+                            string varförkanintestringvaraintjahatarprogramering = näraminor.ToString();
+                            g.DrawString(varförkanintestringvaraintjahatarprogramering, minefont, Blank, i * 40, j * 40);
+                        }
+                        else
+                        {
+                            g.FillRectangle(Blank, i * 40, j * 40, 10, 10);
+                        }
+                    }
+                    if (ruta[i,j] == 2)
+                    {
+                        g.FillRectangle(Flag, i * 40, j * 40, 30, 30);
+                    }
                 }
             }
             
@@ -58,21 +88,51 @@ namespace Slutprojekt_lukas_lindstedt
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Array.Clear(ruta, 0, ruta.Length);
+            Array.Clear(minevärde, 0, ruta.Length);
             int x = 0;
+            antalminor = trackBar1.Value;
             Random kordinat = new Random();
-            while (x <= svårighetsgrad)
+            while (x < antalminor)
             {
-                //random kordinat här
-                //if(kordinaten redan har mina)
+                i = kordinat.Next(0, 8);
+                j = kordinat.Next(0, 8);
+                if (minevärde[i,j] == 0)
+                {
+                    minevärde[i, j] = 1;
+                    x++;
+                }
             }
-            //en while sats används ifall att man får en kordinat som redan har en mina
-            //så kan man skriva så denna loopen inte räknas.
+            
+            panel1.Invalidate();
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
-
+            int mousex = e.X / 40;
+            int mousey = e.Y / 40;
+            if (e.Button == MouseButtons.Left) { ruta[mousex, mousey] = 1; }
+            if (e.Button == MouseButtons.Right) { ruta[mousex, mousey] = 2; }
             panel1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    ruta[i, j] = 1;
+                    
+                }
+            }
+            panel1.Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            antalminor = trackBar1.Value;
+            textBox1.Text = antalminor.ToString();
         }
     }
 }
